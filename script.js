59a1729d9761825f7d9c337fcca19e2c47f687c8
XMANZ <151450043+PRIMANSAH@users.noreply.github.com>
2024-09-22 15:29:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+  
+    console.log("Message Sent:");
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Message:", message);
+  
+    alert("Thank you, " + name + "! Your message has been sent.");
+  });
+  
